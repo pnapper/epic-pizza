3d1e2696074691ac22a4faa82d279ccad4db5582
Patrick Napper <patrick@patricknapper.com>
2017-07-14 17:24:25 -0700

    creates additional javascript business logic and creates html columns to show sizes and pricing.

---
+++ README.md
@@ -0,0 +1,41 @@
+# Epic Pizza
+
+ #### Created for Epicodus class project, 07/14/17
+
+ #### By **Patrick Napper**
+
+ ## Description
+
+ A website for a pizza company where a user can choose one or more individual toppings (cheese, pepperoni, artichoke, anchovy, etc) and a size to order a pizza and see the final cost.
+
+ ## Specifications
+
+ * The user can choose a size for their pizza.
+  - **_Example Input: Small_**
+  - **_Example Output: $10_**
+ * The user can choose toppings for their pizza.
+  - **_Example Input: Pepperoni**
+  - **_Example Output: $13_**
+ * The user can place their order and receive back a total price.
+  - **_Example Input: Press "Place Order" button**
+  - **_Example Output: $13_**
+
+ ## GitHub Pages address: https://pnapper.github.io/epic-pizza/
+
+ ## Setup
+
+ * _Clone this repository_
+ * _Open index.html in any browser_
+ * _Enjoy_
+
+ ## Technologies Used
+
+ HTML
+ CSS
+ Bootstrap
+ JavaScript
+ jQuery
+
+ Copyright (c) 2017 **_Patrick Napper Inc._**
+
+ This software is licensed under the MIT license.
--- js/scripts.js
+++ js/scripts.js
@@ -5,23 +5,48 @@ function Pizza (size, toppings, price) {
 }
 
 Pizza.prototype.cost = function(price) {
-  this.cost += price;
+  return basePrice + toppings;
 }
-
-
-
-
 
 
 $(document).ready(function() {
   $("form#pizza-order").submit(function(event) {
     event.preventDefault();
+    var inputtedSize = $("input[name=size]:checked").val();
+    var basePrice = "";
+    if (inputtedSize === "small") {
+      basePrice = "10";
+    } else if (inputtedSize === "medium") {
+      basePrice = "15";
+    } else {
+      basePrice = "20";
+    }
+    //alert(basePrice);
+    $("#topping").each(function() {
+      var toppings = "";
+      if (inputtedSize === "small") {
+        toppings = "2.50";
+      } else if (inputtedSize === "medium") {
+        toppings = "2.75";
+      } else {
+        toppings = "3"
+      }
+      //newOrder.toppings.push(price)
+      alert(toppings);
+    })
 
-    var order = new Pizza ($("#price").val());
-    var inputtedSize = $("#size").val();
-    var inputtedToppings = $("#topping").val();
+    var inputtedToppings = [];
+    $("input:checkbox[name=topping]:checked").each(function(){
+      var orderToppings = $(this).val();
+      inputtedToppings.push(toppings);
+    });
+    var newOrder = new Pizza (basePrice + inputtedToppings.length * toppings);
+    //alert(inputtedToppings);
+
+    newOrder.cost.push(price);
 
     $("#receipt").show();
+
 
   });
 });
