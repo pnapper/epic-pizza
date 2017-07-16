c9ca4b8d7a60763ad56b110abe3f05ecdc83ed00
Patrick Napper <patrick@patricknapper.com>
2017-07-16 13:06:02 -0700

    fix jQuery to show price when order is submitted and additional styling for receipt.

--- js/scripts.js
+++ js/scripts.js
@@ -12,42 +12,39 @@ Pizza.prototype.cost = function() {
   return basePrice + toppings * inputtedToppings.length;
 }
 
-
 $(document).ready(function() {
   $("form#pizza-order").submit(function(event) {
     event.preventDefault();
     var inputtedSize = $("input[name=size]:checked").val();
-
     if (inputtedSize === "small") {
-      basePrice = 10;
+      basePrice = 10.00;
     } else if (inputtedSize === "medium") {
-      basePrice = 15;
+      basePrice = 15.00;
     } else {
-      basePrice = 20;
+      basePrice = 20.00;
     }
-    alert(basePrice);
-
-      if (inputtedSize === "small") {
-        toppings = 2.50;
-      } else if (inputtedSize === "medium") {
-        toppings = 2.75;
-      } else {
-        toppings = 3;
-      }
-      alert(toppings);
+    //alert(basePrice);
+    if (inputtedSize === "small") {
+      toppings = 2.50;
+    } else if (inputtedSize === "medium") {
+      toppings = 2.75;
+    } else {
+      toppings = 3.00;
+    }
+    //alert(toppings);
 
     $("input:checkbox[name=topping]:checked").each(function(){
       var orderToppings = $(this).val();
       inputtedToppings.push(orderToppings);
       console.log(inputtedToppings);
 
-    var newOrder = new Pizza (basePrice, orderToppings, price);
-    console.log(newOrder.cost());
+      var newOrder = new Pizza (inputtedSize, inputtedToppings, this.price);
+      console.log(newOrder.cost());
 
-    newOrder.cost(newOrder);
-    $("#price").text(newOrder.price);
+      newOrder.cost();
+      $("#price").text(newOrder.cost);
 
-    $("#receipt").show();
+      $("#receipt").show();
     });
   });
 });
